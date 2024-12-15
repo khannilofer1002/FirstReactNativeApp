24d1f65772c699edae4c8041ad09fb62d271c0d1
khannilofer1002 <khannilofer1002@gmail.com>
2024-12-15 18:59:03 +0530

    View, BUtton,Text

---
+++ App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Button, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>React Native mobile App Developer</Text>
+      <Text style={{ fontSize: 30 }}>By Nilofer</Text>
+      <Button title="Press Here" />
+    </View>
+  );
+};
+
+export default App;
